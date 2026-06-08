0447e0d956f5a4175ba05037ef0478fd750e4a50
tendaningatana-glitch <tendaningatana@gmail.com>
2026-06-08 18:04:41 +0000

    Advanced Coding

--- Advanced Coding on retail sales.sql
+++ Advanced Coding on retail sales.sql
@@ -1,11 +1,11 @@
 -- Databricks notebook source
 -- use brightlearn.data;
 SELECT *
-FROM retail_sales;
+FROM retail_sales
 
 -- Which product categories generated more than 150,000 in total sales?
 SELECT `Product Category`,
-       SUM(`Total Amount`) AS total_sales
+SUM(`Total Amount`) AS total_sales
 FROM retail_sales
 GROUP BY `Product Category`
 HAVING total_sales > 150000;
@@ -20,20 +20,35 @@ HAVING avg_transect > 300
 -- Find the top 5 customers with the highest total spending.
 
  SELECT `Customer ID `,
-       Total Amount
 FROM retail_sales
 ORDER BY`total Amount`DESC
 LIMIT 5;
 
-
 -- Which product categories have more than 300 transactions?
-SELECT 
+
+SELECT `Product Category`,
+      COUNT(Transaction) As total_transactions
+FROM retail_sales
+GROUP BY `Product Category`
+HAVING COUNT(Transaction)>300;
+
 
 -- What is the highest sale amount recorded for each product category?
 
+SELECT `Product Category`,
+      MAX(`Sale Amount`) AS highest_sale 
+FROM retail_sales
+GROUP BY Product category
+
+
 -- Find the minimum sale amount for each gender.
+SELECT Gender
+      Min(Sale Amount) AS Lowest sale 
+      FROM retail sale 
+      GROUP BY total Amount ASC
 
 -- Show categories where the average quantity sold is greater than 2.
+
 
 -- Find the total sales for customers aged between 25 and 40 who purchased Beauty or Clothing products.
 
